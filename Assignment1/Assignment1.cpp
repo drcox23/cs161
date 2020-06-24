8202b2eda18c8ed624b4d4557190f6dbc04741a5
drcox23 <drcox23@gmail.com>
2020-06-23 22:02:17 -1000

    adding hello world assignment to github

---
+++ Assignment1/Assignment1.cpp
@@ -0,0 +1,26 @@
+// Assignment1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//This program shall print my name in the terminal
+// Doug Cox - 06/23/2020
+// I started this program from Visual Studio, selecting a new terminal project. I have also tied the source control of the project to my Git repository
+// by selecting this project type in Visual Studio, it auto-populates the code to output "Hello World". Updating to output "Hello Doug"
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Doug!\n" << endl;
+
+    return (0);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
