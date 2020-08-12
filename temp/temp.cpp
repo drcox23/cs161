29630d60382fe81879a0f8bdc5bd48e7633a1cec
drcox23 <drcox23@gmail.com>
2020-08-12 10:40:59 -1000

    initial commit for temp project

---
+++ temp/temp.cpp
@@ -0,0 +1,20 @@
+// Douglas Cox
+// Assignment 8
+// 08/13/2020
+// Source: TBD
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
+
+
